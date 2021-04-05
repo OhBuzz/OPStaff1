1c4fc4f852c84f92fd8afe29e1ffa07898cb83c1
Samkist <samshark23451@gmail.com>
2021-04-05 00:08:14 -0400

    toggle-fix

--- src/main/java/net/buzz/listener/StaffListeners.java
+++ src/main/java/net/buzz/listener/StaffListeners.java
@@ -25,15 +25,15 @@ public class StaffListeners implements Listener {
     public void StaffConnect(ServerConnectEvent event) {
         ProxiedPlayer player = event.getPlayer();
         try {
-            for (ProxiedPlayer online : ProxyServer.getInstance().getPlayers()) {
-                if (player.hasPermission("opcraft.staff") &&
-                online.hasPermission("opcraft.staff")) {
-                    if (this.instance.toggle.contains(player))
-                        return;
-                    online.sendMessage(ChatUtil.colorize(this.instance.getLanguageConfiguration().getString("Events.staffConnect"))
-                    .replace("{user}", player.getName())
-                    .replace("{server}", String.valueOf(event.getTarget().getName())));
-                }
+            if(this.instance.toggle.contains(player)) {
+                return;
+            }
+            final boolean isStaff = player.hasPermission("opcraft.staff");
+            if(isStaff) {
+                ProxyServer.getInstance().getPlayers().forEach(online ->
+                        online.sendMessage(ChatUtil.colorize(this.instance.getLanguageConfiguration().getString("Events.staffConnect"))
+                        .replace("{user}", player.getName())
+                        .replace("{server}", String.valueOf(event.getTarget().getName()))));
             }
         } catch (NullPointerException nullPointerException) {
             nullPointerException.printStackTrace();
